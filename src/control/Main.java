749a80a8af87b289b649e0e125063b8eebab69e5
ayalkh <118622325+ayalkh@users.noreply.github.com>
2024-01-30 20:19:58 +0200

    second request
    Signed-off-by: ayalkh <118622325+ayalkh@users.noreply.github.com>

--- src/control/Main.java
+++ src/control/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			//first request
+			//second request
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
