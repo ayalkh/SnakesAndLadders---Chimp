f30c3c638e9a39e7270acebf5a88117dcb2bcbbb
sireen23 <sireenansary@gmail.com>
2024-02-02 21:36:00 +0200

    files changed

--- src/control/HistoryBoardController.java
+++ src/control/HistoryBoardController.java
@@ -1,10 +1,31 @@
 package control;
 
-public class HistoryBoardController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class HistoryBoardController extends Application {
 
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/HistoryBoard.fxml"));
+			Parent root = loader.load();
+
+			WelcomePageController controller = loader.getController();
+			// You can perform additional setup on the controller if needed
+
+			Scene scene = new Scene(root, 700, 400);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
