f405f5857632ea76bad017e2f9f88ec628922fc4
ayalkh <118622325+ayalkh@users.noreply.github.com>
2024-02-04 21:24:31 +0200

    adding navigation between screens

--- src/control/WelcomePageController.java
+++ src/control/WelcomePageController.java
@@ -1,13 +1,34 @@
 package control; // Change this to your actual package name
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class WelcomePageController extends Application {
+    @FXML
+    private Button viewQuestion;
+    @FXML
+    private void handleViewQuestionAction(ActionEvent event) {
+        try {
+            // Load the QuestionsList view
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/QuestionsList.fxml"));
+            Parent questionsListView = loader.load();
 
+            // Get the current stage using the event's source
+            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+            // Set the scene to the questionsList view
+            stage.setScene(new Scene(questionsListView));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 	@Override
 	public void start(Stage primaryStage) {
 		try {
