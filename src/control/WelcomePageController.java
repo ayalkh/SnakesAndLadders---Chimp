ad52c94977aff946ec2119003e711d2e3cdbe997
sireen23 <sireenansary@gmail.com>
2024-02-12 14:28:03 +0200

    test

--- src/control/WelcomePageController.java
+++ src/control/WelcomePageController.java
@@ -1,5 +1,4 @@
-package control; 
-
+package control;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -9,25 +8,27 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
-public class WelcomePageController  {
-    @FXML
-    private Button viewQuestion;
-    @FXML
-    private void handleViewQuestionAction(ActionEvent event) {
-        try {
-            // Load the QuestionsList view
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/QuestionsList.fxml"));
-            Parent questionsListView = loader.load();
+//a
+public class WelcomePageController {
+	@FXML
+	private Button viewQuestion;
 
-            // Get the current stage using the event's source
-            Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+	@FXML
+	private void handleViewQuestionAction(ActionEvent event) {
+		try {
+			// Load the QuestionsList view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/QuestionsList.fxml"));
+			Parent questionsListView = loader.load();
 
-            // Set the scene to the questionsList view
-            stage.setScene(new Scene(questionsListView));
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+			// Get the current stage using the event's source
+			Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+
+			// Set the scene to the questionsList view
+			stage.setScene(new Scene(questionsListView));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 }
