d72a87119389875e22402245e9bd8985e3f4f88f
sireen23 <sireenansary@gmail.com>
2024-02-16 16:50:03 +0200

    objects disappear
    change the object so they disappear when clicking

--- src/control/NumberOfPlayersController.java
+++ src/control/NumberOfPlayersController.java
@@ -9,8 +9,6 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -41,7 +39,20 @@ public class NumberOfPlayersController {
 	@FXML
 	void choose2(MouseEvent event) {
 		number = 2;
-		showAlert("Group 1 clicked", "You clicked on group 1!");
+		try {
+			// Load the PlayersInfo view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayersInfo.fxml"));
+			Parent x = loader.load();
+
+			// Get the current stage using the event's source
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			// Set the scene to the PlayersInfo view
+			stage.setScene(new Scene(x));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -68,19 +79,59 @@ public class NumberOfPlayersController {
 	@FXML
 	void choose4(MouseEvent event) {
 		number = 4;
-		Alerts.alertBox(AlertType.ERROR, "", "You have to select an answer", null);
+		try {
+			// Load the PlayersInfo view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayersInfo.fxml"));
+			Parent x = loader.load();
+
+			// Get the current stage using the event's source
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			// Set the scene to the PlayersInfo view
+			stage.setScene(new Scene(x));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	@FXML
 	void choose5(MouseEvent event) {
 		number = 5;
+		try {
+			// Load the PlayersInfo view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayersInfo.fxml"));
+			Parent x = loader.load();
 
+			// Get the current stage using the event's source
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			// Set the scene to the PlayersInfo view
+			stage.setScene(new Scene(x));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
 	void choose6(MouseEvent event) {
 		number = 6;
+		try {
+			// Load the PlayersInfo view
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayersInfo.fxml"));
+			Parent x = loader.load();
+
+			// Get the current stage using the event's source
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+			// Set the scene to the PlayersInfo view
+			stage.setScene(new Scene(x));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 	}
 
@@ -94,12 +145,20 @@ public class NumberOfPlayersController {
 
 	}
 
-	private void showAlert(String title, String content) {
-		Alert alert = new Alert(AlertType.INFORMATION);
-		alert.setTitle(title);
-		alert.setHeaderText(null);
-		alert.setContentText(content);
-		alert.showAndWait();
+	private void loadPlayersInfoView(MouseEvent event) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayersInfo.fxml"));
+			Parent x = loader.load();
+
+			PlayersInfoControl playersInfoController = loader.getController();
+			playersInfoController.setNumberOfPlayers(number);
+
+			Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			stage.setScene(new Scene(x));
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/control/PlayersInfoControl.java
+++ src/control/PlayersInfoControl.java
@@ -1,22 +1,77 @@
 
 package control;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.Cursor;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.ImageView;
 
 public class PlayersInfoControl {
 
 	@FXML
 	private ResourceBundle resources;
-
+	private String selectedValue;
 	@FXML
 	private URL location;
+	@FXML
+	private ComboBox<String> comboBox;
+	private int numberOfPlayers;
 
 	@FXML
-	void initialize() {
+	private ImageView redObject;
+
+	@FXML
+	private ImageView greenObject;
+
+	public void setNumberOfPlayers(int number) {
+		this.numberOfPlayers = number;
+		// You can now use numberOfPlayers in this controller
+	}
+
+	@FXML
+	void clickRedObject(MouseEvent event) {
 
 	}
 
+	@FXML
+	void initialize() {
+		comboBox.setItems(FXCollections.observableArrayList("easy", "medium", "hard"));
+
+		comboBox.valueProperty().addListener(new ChangeListener<String>() {
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+				// newValue holds the newly selected item
+				selectedValue = newValue;
+				saveSelectedValue(newValue);
+			}
+		});
+		// Change cursor when mouse enters the ImageView
+		redObject.setOnMouseEntered(event -> redObject.setCursor(Cursor.HAND));
+
+		// Optionally, set cursor back to default when mouse exits the ImageView
+		redObject.setOnMouseExited(event -> redObject.setCursor(Cursor.DEFAULT));
+
+		redObject.setOnMouseClicked(event -> {
+			// Set the visibility to false when the ImageView is clicked
+			redObject.setVisible(false);
+		});
+
+		greenObject.setOnMouseClicked(event -> {
+			// Set the visibility to false when the ImageView is clicked
+			greenObject.setVisible(false);
+		});
+
+	}
+
+	private void saveSelectedValue(String value) {
+		// Implement your saving logic here
+		System.out.println("Selected value: " + value);
+	}
 }
