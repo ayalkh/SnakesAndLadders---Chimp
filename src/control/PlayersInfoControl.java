f7628797a10c48f1ca1aedee7caab1d02d14176b
ayalkh <118622325+ayalkh@users.noreply.github.com>
2024-02-17 00:00:31 +0200

    Revert "Revert "changes""
    This reverts commit 43bf8eacab4d2527a2e47e53ce722397d9a94f11.

---
+++ src/control/PlayersInfoControl.java
@@ -0,0 +1,22 @@
+
+package control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+
+public class PlayersInfoControl {
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
+	private URL location;
+
+	@FXML
+	void initialize() {
+
+	}
+
+}
