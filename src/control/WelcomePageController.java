d94ec72a80d2bf8e2798c0b5a525ce8b1ef6e3d4
sireen23 <sireenansary@gmail.com>
2024-02-26 13:49:21 +0200

    welcome page chages
    1.change the path from WelcomePage t QuestionList
    2.change the buttons that start the game (from button to imageview)
    3.edit the navigation so when opening new window the previous window closed

--- src/control/WelcomePageController.java
+++ src/control/WelcomePageController.java
@@ -1,11 +1,13 @@
 package control;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Cursor;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class WelcomePageController {
@@ -13,40 +15,60 @@ public class WelcomePageController {
 	private Button viewQuestion;
 	@FXML
 	private Button startGame;
+	@FXML
+	private ImageView start;
 
 	@FXML
-	private void handleViewQuestionAction(ActionEvent event) {
+	private ImageView viewQuestionImage;
+
+	@FXML
+	void initialize() {
+		start.setCursor(Cursor.HAND);
+		viewQuestionImage.setCursor(Cursor.HAND);
+
+		// Set opacity effects for 'start'
+		start.setOnMouseEntered(event -> start.setOpacity(0.8));
+		start.setOnMouseExited(event -> start.setOpacity(1.5));
+
+		// Set opacity effects for 'viewQuestionImage'
+		viewQuestionImage.setOnMouseEntered(event -> viewQuestionImage.setOpacity(0.8));
+		viewQuestionImage.setOnMouseExited(event -> viewQuestionImage.setOpacity(1.5));
+	}
+
+	@FXML
+	private void handleViewQuestionImageClick(MouseEvent event) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/WelcomePage.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/QuestionsList.fxml")); // Change to the
+																									// correct path
 			Parent questionsListView = loader.load();
 
-			// Get the current stage using the event's source
-			Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+			Stage newStage = new Stage();
+			newStage.setScene(new Scene(questionsListView));
+			newStage.show();
 
-			// Set the scene to the questionsList view
-			stage.setScene(new Scene(questionsListView));
-			stage.show();
+			// Close the current stage
+			Stage currentStage = (Stage) viewQuestionImage.getScene().getWindow();
+			currentStage.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
 	@FXML
-	void handlestartGame(ActionEvent event) {
+	void handleStartImageClick(MouseEvent event) {
 		try {
-			// Load the QuestionsList view
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/popUpMessages/NumberOfPlayers.fxml"));
 			Parent players = loader.load();
 
-			// Get the current stage using the event's source
-			Stage stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+			Stage newStage = new Stage();
+			newStage.setScene(new Scene(players));
+			newStage.show();
 
-			// Set the scene to the questionsList view
-			stage.setScene(new Scene(players));
-			stage.show();
+			// Close the current stage
+			Stage currentStage = (Stage) start.getScene().getWindow();
+			currentStage.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
