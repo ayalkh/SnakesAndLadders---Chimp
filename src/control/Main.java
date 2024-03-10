16feac8c13a404bee1aa0ddb8ace291ad349064e
sireen23 <sireenansary@gmail.com>
2024-03-10 16:11:35 +0200

    adding sounds and
    adding sounds and editing questionpop design

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,8 +1,6 @@
 package control;
 
-import javafx.application.Application
-
-;
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -12,8 +10,8 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) {
+		BackgroundMusicPlayer.getInstance().playMusic("backGroungMusic.mp3");
 		try {
-
 			AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/view/WelcomePage.fxml"));
 			Scene scene = new Scene(root, 1192, 680);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
@@ -26,6 +24,12 @@ public class Main extends Application {
 	}
 
 	public static void main(String[] args) {
+
 		launch(args);
 	}
 }
+
+//music credit : Run Amok by Kevin MacLeod | https://incompetech.com/
+//Music promoted by https://www.chosic.com/free-music/all/
+//Creative Commons CC BY 3.0
+//https://creativecommons.org/licenses/by/3.0/
