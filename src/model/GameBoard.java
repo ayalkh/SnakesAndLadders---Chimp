1f1e78741cb235852eb78e164bd1a8b4c891eec8
leenaz2442 <151503700+leenaz2442@users.noreply.github.com>
2024-03-10 22:39:21 +0200

    Changes

---
+++ src/model/GameBoard.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.util.*;
+
+public abstract class GameBoard {
+    protected Tile[][] board;
+    protected int size; // Varies by game difficulty
+    protected Random random = new Random();
+    protected List<Player> gamePlayers = new ArrayList<>();
+    protected Map<Integer, Snake> snakesMap = new HashMap<>();
+    protected Map<Integer, Ladder> laddersMap = new HashMap<>();
+    protected List<QuestionTile> questions = new ArrayList<>();
+
+    public GameBoard(int size) {
+        this.size = size;
+        this.board = new Tile[size][size];
+    }
+
+    public GameBoard() {
+       
+    }
+
+    protected void initializeBoard() {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                board[i][j] = new Tile(i, j);
+            }
+        }
+    }
+
+    protected abstract void placeSnakes();
+    protected abstract void placeLadders();
+    protected abstract void placequestions();
+   
+    // Other common methods and abstract methods as necessary
+
+	protected void placeSnakes1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected void placeLadders1() {
+		// TODO Auto-generated method stub
+		
+	}
+}
